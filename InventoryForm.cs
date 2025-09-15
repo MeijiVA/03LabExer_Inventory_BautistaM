5e540c865e55d244214df09fb3287f9fcb96cbd3
MeijiVA <mizaelantonbautista@gmail.com>
2025-09-15 21:03:46 +0800

    Added New Exceptions and TryCatch for btnAddProduct_Click

--- InventoryForm.cs
+++ InventoryForm.cs
@@ -13,6 +13,30 @@ namespace _03LabExer_Inventory_BautistaM
 {
     public partial class frmAddProduct : Form
     {
+        //CHALLENGE EXER STEP 12
+        public class NumberFormatException : Exception
+        {
+            public NumberFormatException(string message) : base(message)
+            {
+
+            }
+        }
+        public class StringFormatException : Exception
+        {
+            public StringFormatException(string message) : base(message)
+            {
+
+            }
+        }
+        public class CurrencyFormatException : Exception
+        {
+            public CurrencyFormatException(string message) : base(message)
+            {
+
+            }
+        }
+
+
         //STEP 5
         private string _ProductName, _Category, _MfgDate, _ExpDate, _Description;
         private int _Quantity;
@@ -28,17 +52,34 @@ namespace _03LabExer_Inventory_BautistaM
 
         private void btnAddProduct_Click(object sender, EventArgs e)
         {
-            //STEP 10
-            _ProductName = Product_Name(txtProductName.Text);
-            _Category = cbCategory.Text;
-            _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
-            _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
-            _Description = richTxtDescription.Text;
-            _Quantity = Quantity(txtQuantity.Text);
-            _SellPrice = SellingPrice(txtSellPrice.Text);
-            showProductList.Add(new ProductClass(_ProductName,_Category,_MfgDate,_ExpDate,_SellPrice,_Quantity,_Description));
-            gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            gridViewProductList.DataSource = showProductList;
+            //CHALLENGE EXER 13
+            try
+            {
+                //STEP 10
+                _ProductName = Product_Name(txtProductName.Text);
+                _Category = cbCategory.Text;
+                _MfgDate = dtPickerMfgDate.Value.ToString("yyyy-MM-dd");
+                _ExpDate = dtPickerExpDate.Value.ToString("yyyy-MM-dd");
+                _Description = richTxtDescription.Text;
+                _Quantity = Quantity(txtQuantity.Text);
+                _SellPrice = SellingPrice(txtSellPrice.Text);
+                showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate, _ExpDate, _SellPrice, _Quantity, _Description));
+                gridViewProductList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                gridViewProductList.DataSource = showProductList;
+            }
+            catch (StringFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (NumberFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (CurrencyFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
 
         private void frmAddProduct_Load(object sender, EventArgs e)
@@ -58,21 +99,21 @@ namespace _03LabExer_Inventory_BautistaM
         {
             if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
                 //Exception here
-                throw new Exception();
+                throw new StringFormatException("Invalid Name Format");
                 return name;
         }
         public int Quantity(string qty)
         {
             if (!Regex.IsMatch(qty, @"^[0-9]"))
                 //Exception here
-                throw new Exception();
+                throw new NumberFormatException("Invalid Number Format");
             return Convert.ToInt32(qty);
         }
         public double SellingPrice(string price)
         {
             if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
                 //Exception here
-                throw new Exception();
+                throw new CurrencyFormatException("Invalid Currency Format");
             return Convert.ToDouble(price);
         }
 
