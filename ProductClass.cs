a621c37b03ad3608ee5e948020e3e3dc932b16f3
MeijiVA <mizaelantonbautista@gmail.com>
2025-09-15 20:12:05 +0800

    Step 4 and 5 Code added (Accessor. Mutator. Instance Variable, and Constructor)

---
+++ ProductClass.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03LabExer_Inventory_BautistaM
+{
+
+    internal class ProductClass
+    {
+        //STEP 4
+        private int _Quantity;
+        private double _SellingPrice;
+        private string _ProductName, _Category, _ManufacturingDate, _ExpirationDate, _Description;
+
+        public ProductClass(string ProductName, string Category, string MfgDate, string ExpDate, double Price, int Quantity, string Description)
+        {
+            this._Quantity = Quantity;
+            this._SellingPrice = Price;
+            this._ProductName = ProductName;
+            this._Category = Category; 
+            this._ManufacturingDate = MfgDate; 
+            this._ExpirationDate = ExpDate;
+            this._Description = Description;
+        }
+
+
+        //STEP 5
+        public string productName
+        {
+            get
+            {
+                return this._ProductName;
+            }
+            set
+            {
+                this._ProductName = value;
+            }
+        }// accessor _ProductName
+
+        public string category
+        {
+            get
+            {
+                return this._Category;
+            }
+            set
+            {
+                this._Category = value;
+            }
+        }// accessor _Category
+
+        public string manufacturingDate
+        {
+            get
+            {
+                return this._ManufacturingDate;
+            }
+            set
+            {
+                this._ManufacturingDate = value;
+            }
+        }// accessor _ManufacturingDate
+
+        public string expirationDate
+        {
+            get
+            {
+                return this._ExpirationDate;
+            }
+            set
+            {
+                this._ExpirationDate = value;
+            }
+        }// accessor _ExpirationDate
+
+        public string description
+        {
+            get
+            {
+                return this._Description;
+            }
+            set
+            {
+                this._Description = value;
+            }
+        }// accessor _Description
+
+        public int quantity
+        {
+            get
+            {
+                return this._Quantity;
+            }
+            set
+            {
+                this._Quantity = value;
+            }
+        }// accessor _Quantity
+
+        public double sellingPrice
+        {
+            get
+            {
+                return this._SellingPrice;
+            }
+            set
+            {
+                this._SellingPrice = value;
+            }
+        }// accessor _SellingPrice
+
+    }
+}
